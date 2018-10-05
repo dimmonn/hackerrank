2efac430fd21669d8c8361e82f34df8f1d43a935
dpolishc <dpolishc@cisco.com>
2018-10-05 20:10:23 +0200

    jumping on the clouds

--- src/main/java/algos/hackerrank/JumpingOnTheClouds.java
+++ src/main/java/algos/hackerrank/JumpingOnTheClouds.java
@@ -5,7 +5,6 @@ public class JumpingOnTheClouds {
         int count = 0;
         for (int i = 0; i < c.length - 1; i++) {
             if (i < c.length - 2 && c[i + 2] == 0) {
-                //count ++;
                 i++;
 
             }
