58196c649de568a816b4f4a0e0daea8456a6a4cd
dpolishc <dpolishc@cisco.com>
2018-10-06 21:31:32 +0200

    2darrdc

---
+++ src/main/java/algos/hackerrank/easy/TwoDarrayDC.java
@@ -0,0 +1,26 @@
+package algos.hackerrank.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TwoDarrayDC {
+    static int hourglassSum(int[][] arr) {
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i < arr.length - 2 && j < arr[i].length - 2) {
+                    int sum;
+                    sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+                    sum = sum + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                    sum = sum + arr[i + 1][j + 1];
+                    res.add(sum);
+                }
+            }
+        }
+        return Collections.max(res);
+    }
+
+
+}
