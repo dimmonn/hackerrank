acd247184d102587386112d1fa35bd9b5fc7f159
dpolishc <dpolishc@cisco.com>
2018-10-08 23:16:02 +0200

    left rotation

---
+++ src/main/java/algos/hackerrank/easy/LeftRotation.java
@@ -0,0 +1,22 @@
+package algos.hackerrank.easy;
+
+import java.util.Arrays;
+
+public class LeftRotation {
+    static int[] rotLeft(int[] a, int d) {
+
+        int[] result = new int[a.length];
+        int delta = d - d / a.length;
+        int count = 0;
+        int[] left = Arrays.copyOfRange(a, 0, delta);
+        int[] right = Arrays.copyOfRange(a, delta, a.length);
+        for (int i = 0; i < right.length; i++) {
+            result[i] = right[i];
+            count++;
+        }
+        for (int aLeft : left) {
+            result[count++] = aLeft;
+        }
+        return result;
+    }
+}
