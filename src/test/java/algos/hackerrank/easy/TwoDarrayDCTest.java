cf48180c63e002e578a18d518e2afd301fb6848f
dpolishc <dpolishc@cisco.com>
2018-12-24 08:19:45 +0100

    test

---
+++ src/test/java/algos/hackerrank/easy/TwoDarrayDCTest.java
@@ -0,0 +1,23 @@
+package algos.hackerrank.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class TwoDarrayDCTest {
+
+    @org.junit.Test
+    public void hourglassSum() throws Exception {
+
+        Optional<String> optionalS = Optional.ofNullable(null);
+        String a = optionalS.orElseThrow(() -> new Exception());
+        System.out.println(a);
+        List<String> stringList = new ArrayList<>();
+        stringList.add("q");
+        stringList.add("w");
+        stringList.add("e");
+        stringList.add("r");
+
+
+    }
+}
