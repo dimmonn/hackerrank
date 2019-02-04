3a6f5957df28e44404cb068f253efe68edcf5ea0
dpolishc <dpolishc@cisco.com>
2019-02-04 19:10:52 +0100

    queries

--- src/main/java/algos/hackerrank/medium/FrequencyQueries.java
+++ src/main/java/algos/hackerrank/medium/FrequencyQueries.java
@@ -1,53 +1,76 @@
 package algos.hackerrank.medium;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class FrequencyQueries {
-    static List<Integer> freqQuery(List<List<Integer>> queries) {
+    static List<Integer> freqQuery(List<int[]> queries) {
         List<Integer> integers = new ArrayList<>();
         Map<Integer, Integer> map = new HashMap<>();
         Map<Integer, Integer> friequency = new HashMap<>();
 
-        queries.forEach(list -> {
-            if (list.get(0) == 1) {
-                map.put(list.get(1), map.getOrDefault(list.get(1), 0) + 1);
-                friequency.put(map.get(list.get(1)),
-                        friequency.getOrDefault(
-                                map.get(list.get(1)), 0) + 1);
-            } else if (list.get(0) == 2) {
+        for (int[] list : queries) {
+            if (list[0] == 1) {
 
-                if (map.get(list.get(1)) != null && map.get(list.get(1)) != 0) {
-                    map.put(list.get(1), map.get(list.get(1)) - 1);
+                if (map.get(list[1]) != null) {
 
-                    if (map.get(list.get(1)) != 0) {
-                        friequency.put(
-                                map.get(list.get(1)),
-                                friequency.get(map.get(list.get(1))) - 1);
-                    }
-
-                } else if (map.get(list.get(1)) != null && map.get(list.get(1)) == 0) {
-                    map.remove(list.get(1));
-
-
+                    friequency.put(map.getOrDefault(list[1], 1),
+                            friequency.getOrDefault(
+                                    map.get(list[1]), 2) - 1);
                 }
+
+
+                map.put(list[1], map.getOrDefault(list[1], 0) + 1);
+                friequency.put(map.get(list[1]),
+                        friequency.getOrDefault(
+                                map.get(list[1]), 0) + 1);
+            } else if (list[0] == 2) {
+
+                if (map.get(list[1]) != null && map.get(list[1]) > 0) {
+
+                    friequency.put(map.getOrDefault(list[1], 1), friequency.getOrDefault(map.get(list[1]), 2) - 1);
+
+
+                    map.put(list[1], map.getOrDefault(list[1], 2) - 1);
+
+                    friequency.put(map.get(list[1]), friequency.getOrDefault(map.get(list[1]), 0) + 1);
+                }
+
+
             } else {
-                System.out.println(friequency);
-                if (friequency.get(list.get(1)) != null) {
+
+                if (friequency.get(list[1]) != null && friequency.get(list[1]) > 0) {
                     integers.add(1);
                 } else {
                     integers.add(0);
                 }
             }
-        });
-
+        }
 
         return integers;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+
+        List<int[]> myArray = new ArrayList<>();
+
+        File file = new File("/Users/dpolishc/Downloads/test.txt");
+        Scanner scanner = new Scanner(file);
+
+        for (int row = 0; scanner.hasNextLine() && row < 10000; row++) {
+            String[] s = scanner.nextLine().split(" ");
+            int[] integers = new int[2];
+            integers[0] = Integer.valueOf(s[0]);
+            integers[1] = Integer.valueOf(s[1]);
+            myArray.add(integers);
+        }
+
+
+        System.out.println();
+        freqQuery(myArray);
     }
+
+
 }
