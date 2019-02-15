731810ee3d486481102d5933959ee85c05646144
dpolishc <dpolishc@cisco.com>
2019-02-15 23:58:47 +0100

    fraud activity notifications

---
+++ src/main/java/algos/hackerrank/medium/FraudulentActivityNotifications.java
@@ -0,0 +1,58 @@
+package algos.hackerrank.medium;
+
+import java.util.*;
+
+public class FraudulentActivityNotifications {
+
+    public static void insertToSortedList(List<Integer> array, Integer e) {
+        int left, right, mid;
+
+
+        left = 0;
+        right = array.size();
+
+
+        while (left < right) {
+            mid = (left + right) / 2;
+            int result = ((Comparator<Integer>) Comparator.naturalOrder()).compare(array.get(mid), e);
+
+
+            if (result > 0) { //If e is lower
+                right = mid;
+            } else { //If e is higher
+                left = mid + 1;
+            }
+        }
+
+        array.add(left, e);
+
+    }
+
+    static int activityNotifications(int[] expenditure, int d) {
+        int result = 0;
+        List<Integer> q = new ArrayList<>();
+        List<Integer> tmp = new ArrayList<>();
+        for (int i = 0; i < d; i++) {
+            q.add(expenditure[i]);
+            tmp.add(expenditure[i]);
+
+        }
+        Collections.sort(q);
+        for (int i = d; i < expenditure.length - 1; i++) {
+            double median_ = q.size() % 2 == 0 ? (double) (q.get(q.size() / 2) + q.get((q.size() / 2) - 1)) / 2
+                    : q.get(q.size() / 2);
+            if (expenditure[i] >= median_ * 2) {
+                result++;
+            }
+            int i1 = Collections.binarySearch(q, tmp.get(0));
+            q.remove(i1);
+            tmp.remove(0);
+            tmp.add(expenditure[i]);
+
+            insertToSortedList(q, expenditure[i]);
+
+
+        }
+        return result;
+    }
+}
