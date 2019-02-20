da1af0db32eb0b0b197f673af8c4b05417782e41
dpolishc <dpolishc@cisco.com>
2019-02-20 16:29:03 +0100

    sherlock and the valid string

---
+++ src/main/java/algos/hackerrank/medium/SherlockAndTheValidString.java
@@ -0,0 +1,77 @@
+package algos.hackerrank.medium;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class SherlockAndTheValidString {
+
+    static String isValid(String s) {
+        if (s.length() == 1) {
+            return "YES";
+        }
+        int[] chars = new int[26];
+        char[] chars1 = s.toCharArray();
+
+        for (int c : chars1) {
+            chars[c - 'a']++;
+        }
+        Arrays.sort(chars);
+        int zeroFinishedIndex = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != 0) {
+                zeroFinishedIndex = i;
+                break;
+            }
+
+        }
+        if (zeroFinishedIndex == 0) {
+            return "NO";
+        }
+        int[] ints = Arrays.copyOfRange(chars, zeroFinishedIndex, chars.length);
+
+
+        Map<Integer, Integer> integerIntegerMap = new HashMap<>();
+        for (int anInt : ints) {
+            integerIntegerMap.put(anInt, integerIntegerMap.getOrDefault(anInt, 0) + 1);
+
+        }
+        if (integerIntegerMap.size() > 2) {
+            return "NO";
+        }
+        if (integerIntegerMap.size() == 1) {
+            return "YES";
+        }
+
+        List<Map.Entry<Integer, Integer>> twoElementswithDiff = new ArrayList<>(integerIntegerMap.entrySet());
+        Map.Entry<Integer, Integer> smallest = twoElementswithDiff.get(0).getValue() >
+                twoElementswithDiff.get(1).getValue() ?
+                twoElementswithDiff.get(1) : twoElementswithDiff.get(0);
+
+        twoElementswithDiff.remove(smallest);
+        Map.Entry<Integer, Integer> biggest = twoElementswithDiff.get(0);
+        if (smallest.getKey() == 1 && smallest.getValue() == 1
+                || smallest.getValue() == 1 && Math.abs(smallest.getKey() - biggest.getKey() - 1) == 0
+        ) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("/Users/dpolishc/Downloads/test.log");
+        FileReader fileReader = new FileReader(file);
+        StringBuilder stringBuffer = new StringBuilder();
+        int numCharsRead;
+        char[] charArray = new char[1024];
+        while ((numCharsRead = fileReader.read(charArray)) > 0) {
+            stringBuffer.append(charArray, 0, numCharsRead);
+        }
+        System.out.println(isValid(stringBuffer.toString()));
+    }
+}
