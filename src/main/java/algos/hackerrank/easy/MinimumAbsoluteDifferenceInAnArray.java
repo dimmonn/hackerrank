0a493fc7c16f856137e962af1d98edcb1cd4cd4e
dpolishc <dpolishc@cisco.com>
2019-02-26 22:03:50 +0100

    min abs diff in an array

---
+++ src/main/java/algos/hackerrank/easy/MinimumAbsoluteDifferenceInAnArray.java
@@ -0,0 +1,28 @@
+package algos.hackerrank.easy;
+
+import java.util.Arrays;
+
+public class MinimumAbsoluteDifferenceInAnArray {
+
+    static int minimumAbsoluteDifference(int[] arr) {
+        int result = Integer.MAX_VALUE;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int abs = Math.abs(arr[i] - arr[i + 1]);
+            if (abs == 0) {
+                return 0;
+            }
+            result = result < abs ? result : abs;
+
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{3, -7, 0};
+        System.out.println(minimumAbsoluteDifference(a));
+    }
+}
