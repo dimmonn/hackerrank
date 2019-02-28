761cf7ca53f59fe7f3555b131947c1aba8e6c9e6
dpolishc <dpolishc@cisco.com>
2019-02-28 12:18:54 +0100

    luckBalance

---
+++ src/main/java/algos/hackerrank/easy/LuckBalance.java
@@ -0,0 +1,37 @@
+package algos.hackerrank.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LuckBalance {
+
+    static int luckBalance(int k, int[][] contests) {
+
+        List<Integer> maxes = new ArrayList<>();
+        int result = 0;
+        for (int i = 0; i < contests.length; i++) {
+            if (contests[i][1] == 1) {
+                maxes.add(contests[i][0]);
+            } else {
+                result += contests[i][0];
+            }
+        }
+        Collections.sort(maxes);
+        if (maxes.size() > k) {
+            for (int i = 0; i < maxes.size() - k; i++) {
+                result -= maxes.get(i);
+
+            }
+
+        }
+        int sum = 0;
+        if (!maxes.isEmpty()) {
+            List<Integer> integers = maxes.size() > k ? maxes.subList(maxes.size() - k, maxes.size()) : maxes;
+            sum = integers.stream().mapToInt(e -> e).sum();
+        }
+
+        return result + sum;
+
+    }
+}
